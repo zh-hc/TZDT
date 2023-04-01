b7a5574c851bba486b26a6bcf188ff9adf56e415
zh-hc <zh.hc@qq.com>
2023-04-01 12:59:30 +0800

    将两个接口合并

--- server/server.js
+++ server/server.js
@@ -19,32 +19,20 @@ app.use((req, res, next) => {
 });
 
 
-app.get('/search', (req, res) => {
+app.get('/:type', (req, res) => {
+  const type = req.params.type;
   const keyword = req.query.keyword;
-  const sql = `SELECT * FROM TZDT WHERE question LIKE '%${keyword}%' OR answer LIKE '%${keyword}%' ORDER BY (CASE WHEN question LIKE '${keyword}%' THEN 1 ELSE 0 END) DESC, LENGTH(question) ASC, LENGTH(answer) ASC LIMIT 10`;
+  let sql;
 
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).send('Internal Server Error');
-    } else {
-      rows = rows.map((item) => {
-        const firstPipeIndex = item.question.indexOf('|');
-        const questionBeforePipe = item.question.substring(0, firstPipeIndex);
-        const questionAfterPipe = item.question.substring(firstPipeIndex);
-        const questionBeforePipeWithUnderscores = questionBeforePipe.replace(/\s/g, '_');
-        return {
-          ...item,
-          question: questionBeforePipeWithUnderscores + questionAfterPipe,
-        };
-      });
-      res.json(rows);
-    }
-  });
-});
+  if (type === 'search') {
+    sql = `SELECT * FROM TZDT WHERE question LIKE '%${keyword}%' OR answer LIKE '%${keyword}%' ORDER BY (CASE WHEN question LIKE '${keyword}%' THEN 1 ELSE 0 END) DESC, LENGTH(question) ASC, LENGTH(answer) ASC LIMIT 10`;
+  } else if (type === 'random') {
+    sql = 'SELECT * FROM TZDT ORDER BY RANDOM() LIMIT 10';
+  } else {
+    res.status(400).send('Bad Request');
+    return;
+  }
 
-app.get('/random', (req, res) => {
-  const sql = 'SELECT * FROM TZDT ORDER BY RANDOM() LIMIT 10';
   db.all(sql, [], (err, rows) => {
     if (err) {
       console.error(err.message);
